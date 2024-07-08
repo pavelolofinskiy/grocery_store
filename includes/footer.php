ab7e7974b8d4a42769637ab6d2c2c275144e158f
pavelolofinskiy <pavelolofinskiy@gmail.com>
2024-07-08 17:09:30 +0300

    finished footer section design

--- includes/footer.php
+++ includes/footer.php
@@ -3,55 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
     <link rel="stylesheet" type="text/css" href="/assets/css/footer.css">
 </head>
 
-<footer class="ecommerce-footer">
-    <div class='contact'>
-        <img src='/assets/img/logo.png'>
-        <p>FoodTrove is the biggest market of grocery products. Get your daily needs from our store.<p>
-        <div class='element'>
-            <img src='/assets/img/place.png'>
-            <p>51 Green St.Huntington ohaio beach ontario, NY
-                11746 KY 4783, USA.<p>
+<footer class="footer-section">
+    <div class="ecommerce-footer">
+        <div class='contact'>
+            <img src='/assets/img/logo.png'>
+            <div class='logo-p'>
+                <p>FoodTrove is the biggest market of grocery products. Get your daily needs from our store.<p>
+            </div>
+            <div class='element'>
+                <div class='img-container'>
+                    <img src='/assets/img/place.png'>
+                </div>    
+                <p>51 Green St.Huntington ohaio beach ontario, NY 
+                    11746 KY 4783, USA.<p>
+            </div>
+            <div class='element'>
+                <div class='img-container'>
+                    <img src='/assets/img/mail.png'>
+                </div>  
+                <p>foodtrove@email.com<p>
+            </div>
+            <div class='element'>
+                <div class='img-container'>
+                    <img src='/assets/img/phone.png'>
+                </div>  
+                <p>+91 123 4567890<p>
+            </div>
         </div>
-        <div class='element'>
-            <img src='/assets/img/mail.png'>
-            <p>foodtrove@email.com<p>
-        </div>
-        <div class='element'>
-            <img src='/assets/img/phone.png'>
-            <p>+91 123 4567890<p>
-            
-        </div>
-    </div>
-    <div class="footer-nav">
-        <ul>
+        <div class="footer-nav">
             <p>Company<p>
-            <li><a href="/">Home</a></li>
-            <li><a href="/shop">Shop</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>
-    </div>
-    <div class="footer-categories">
-        <ul>
+            <ul class='spacing'>
+                <li><a href="/">Home</a></li>
+                <li><a href="/shop">Shop</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </div>
+        <div class="footer-categories">
             <p>Category<p>
-            <li><a href="/">Dairy & Bakery</a></li>
-            <li><a href="/shop">Fruits & Vegetable</a></li>
-            <li><a href="/about">Snack & Spice</a></li>
-            <li><a href="/contact">Juice & Drinks</a></li>
-            <li><a href="/contact">Chicken & Meat</a></li>
-            <li><a href="/contact">Fast Food</a></li>
-        </ul>
-    </div>
-    <div class="subscribtion">
-        <p>Subscribe Our Newsletter<p>
-        <form> <input placeholder="Search here..."></input></form>
-        <div>
-            <a href="https://facebook.com"><img src='/assets/img/facebook.png'></a>
-            <a href="https://x.com"><img src='/assets/img/x.png'></a>
-            <a href="https://instagram.com"><img src='/assets/img/inst.png'></a>
+            <ul class='spacing'>
+                <li><a href="/">Dairy & Bakery</a></li>
+                <li><a href="/shop">Fruits & Vegetable</a></li>
+                <li><a href="/about">Snack & Spice</a></li>
+                <li><a href="/contact">Juice & Drinks</a></li>
+                <li><a href="/contact">Chicken & Meat</a></li>
+                <li><a href="/contact">Fast Food</a></li>
+            </ul>
+        </div>
+        <div class="subscription">
+            <p>Follow Us<p>
+            <div class='spacing'>
+                <a href="https://facebook.com"><img src='/assets/img/facebook.png'></a>
+                <a href="https://x.com"><img src='/assets/img/x.png'></a>
+                <a href="https://instagram.com"><img src='/assets/img/inst.png'></a>
+            </div>
         </div>
     </div>
+    <p class='rights'>© 2024 FoodTrove, All rights reserved.</p>
 </footer>
